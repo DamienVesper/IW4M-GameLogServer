42ada87f16162c6b208031dd138ac5ca5b18a173
Damien Vesper <ldamienvesper@gmail.com>
2021-11-21 15:25:02 -0600

    feat: only allow server to run on port 1625

--- runserver.py
+++ runserver.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', '0.0.0.0')
     try:
         PORT = int(environ.get('SERVER_PORT', '1625'))
+
+        init()
+        app.run('0.0.0.0', PORT, debug=False)
     except ValueError:
-        PORT = 5555
-    init()
-    app.run('0.0.0.0', PORT, debug=False)
+        print("Could not bind to port 1625! Are you sure that it is not in use?")
